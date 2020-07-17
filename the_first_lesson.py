9c311b303e58c51a5843645003aed567bfd09b45
yukmioo <1029350710@qq.com>
2020-07-17 21:03:06 +0800

    add the first lesson

---
+++ the_first_lesson.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 16 12:26:53 2020
+
+@author: 10293
+"""
+
+# type 
+
+a=100
+b=12.345
+c="hello world"
+d= True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
